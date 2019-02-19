e7a7a138080c79509cfbec2ea56952950316847b
Ashley Archibald <aarchibald20@gmail.com>
2019-02-19 04:42:06 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
-def alphabetize(arr)
+def alphabetize(arr, rev = false)
   # code here
+  if rev == true
+    arr.sort_by { |item1, item2| item2 <=> item1 }
+else
+    arr.sort_by { |item1, item2| item1 <=> item2 }
+  end
 end
